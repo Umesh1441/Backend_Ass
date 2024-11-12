00dbadccbca948f266764e30060d6ee5a71dd09c
Umesh1441 <umeshtak34@gmail.com>
2024-11-12 10:03:25 +0530

    chnage

--- index.js
+++ index.js
@@ -12,29 +12,30 @@ dotenv.config({});
 
 const app = express();
 
-// middleware
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'*',
-    credentials:true
-}
 
-app.use(cors());
+// CORS Configuration
+const corsOptions = {
+    origin: "http://localhost:5173", // Your frontend's URL
+    credentials: true,              // Allow cookies and credentials
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); // Handle preflight requests
 
 const PORT = process.env.PORT || 3000;
 
-
-// api's
+// API Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
-app.listen(PORT,()=>{
+// Start Server
+app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at port ${PORT}`);
-})
+});
